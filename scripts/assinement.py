b391ec35f46a8ac851dfbada2581d2894be80a7e
mewmew271 <37979148+mewmew271@users.noreply.github.com>
2019-11-27 17:29:10 +0000

    Add files via upload

--- scripts/assinement.py
+++ scripts/assinement.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
-
+#sudo pip2 install imutils
+import imutils
 # Python libs
 import sys, time
 
@@ -22,55 +22,80 @@ from geometry_msgs.msg import Twist
 
    
 class Robot_main:
-    def __init__(self):
-        self.bridge = CvBridge()
+	def __init__(self):
+		self.bridge = CvBridge()
 
-        self.camera_info_sub = rospy.Subscriber('/thorvald_001/kinect2_camera/hd/camera_info', 
-            CameraInfo, self.camera_info_callback)
+		self.camera_info_sub = rospy.Subscriber('/thorvald_001/kinect2_camera/hd/camera_info', 
+			CameraInfo, self.camera_info_callback)
 
-        rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",
-            Image, self.image_callback)
+		rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",
+			Image, self.image_callback)
 
-        self.tf_listener = tf.TransformListener()
+		self.tf_listener = tf.TransformListener()
 
-    def camera_info_callback(self, data):
-        self.camera_model = image_geometry.PinholeCameraModel()
-        self.camera_model.fromCameraInfo(data)
-        self.camera_info_sub.unregister() #Only subscribe once 
-       
+	def camera_info_callback(self, data):
+		self.camera_model = image_geometry.PinholeCameraModel()
+		self.camera_model.fromCameraInfo(data)
+		self.camera_info_sub.unregister() #Only subscribe once 
+	   
   
-      
-    def image_callback(self, data):
-        cv2.namedWindow("window", 1)
-        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-         
-             
-        #all colours not cuttently useing 
-        #lower_colour = numpy.array([0, 0, 0])
-       #upper_colour = numpy.array([255, 255, 255])
-       #mask = cv2.inRange(hsv, lower_colour, upper_colour)        
-        #M = cv2.moments(mask)
-     
-       
-        lower_green = numpy.array([0, 150, 0]) #bgr arrrays 
-        upper_green = numpy.array([255, 255, 255])
-        #lower_green = numpy.array([60, 200, 0]) #bgr arrrays 
-        #upper_green = numpy.array([60, 255, 255])
-        mask = cv2.inRange(hsv_img, lower_green, upper_green)        
-        mGre = cv2.moments(mask)
+	  
+	def image_callback(self, data):
+		cv2.namedWindow("window", 1)
+		cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+		 
+			 
+		#all colours not cuttently useing 
+		#lower_colour = numpy.array([0, 0, 0])
+	   #upper_colour = numpy.array([255, 255, 255])
+	   #mask = cv2.inRange(hsv, lower_colour, upper_colour)        
+		#M = cv2.moments(mask)
 
-        cv_image = cv2.resize(cv_image, (0,0), fx=0.5, fy=0.5)
-        mask = cv2.resize(mask, (0,0), fx=0.5, fy=0.5)
+	 #carrots (basil)
+	 #0, 150, 0
+	 #255, 255, 255
+	 #carrots (weeds)
+	 #40, 10, 30
+	 #100, 130, 110
 
-        cv2.imshow("hsv", mask)
-        cv2.imshow("window", cv_image)
-        print 'running'
-        cv2.waitKey(3)
+	   
+		lower_green = numpy.array([40, 10, 30]) #bgr arrrays 
+		upper_green = numpy.array([80, 100, 70])
+		#lower_green = numpy.array([60, 200, 0]) #bgr arrrays 
+		#upper_green = numpy.array([60, 255, 255])
+		mask = cv2.inRange(hsv_img, lower_green, upper_green)        
+		mGre = cv2.moments(mask)
+		
+		cv_image = cv2.resize(cv_image, (0,0), fx=0.5, fy=0.5)
+		mask = cv2.resize(mask, (0,0), fx=0.5, fy=0.5)
+		mask = cv2.medianBlur(mask, 13)
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
+
+		for c in cnts:
+		# calculate moments of binary image
+			M = cv2.moments(c)
+ 
+		# calculate x,y coordinate of center
+			if M["m10"] or M["m00"] or M["m01"] != 0:
+				cX = int(M["m10"] / M["m00"])
+				cY = int(M["m01"] / M["m00"])
+ 
+			# put text and highlight the center
+				cv2.circle(cv_image, (cX, cY), 5, (255, 255, 255), -1)
+				#cv2.drawContours(mask, c, -1, (0,255,0), 2)
+				#cv2.circle(mask, (cX, cY), 5, (255, 255, 255), -1)
+
+		cv2.imshow("hsv", mask)
+		cv2.imshow("window", cv_image)
+		#print cnts
+		cv2.waitKey(3)
 
 
 
-        
+		
 cv2.startWindowThread()
 rospy.init_node('robot_main')
 robot_main = Robot_main()
